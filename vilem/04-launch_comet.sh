34be13215e8d5c60065ef848ee5d903808d3ba53
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-02-05 10:22:43 +0100

    add plotting script

--- vilem/04-launch_comet.sh
+++ vilem/04-launch_comet.sh
@@ -42,6 +42,21 @@ function sbatch_gpu_big() {
 }
 
 
+function sbatch_gpu_bigg() {
+    JOB_NAME=$1;
+    JOB_WRAP=$2;
+    mkdir -p logs
+
+    sbatch \
+        -J $JOB_NAME --output=logs/%x.out --error=logs/%x.err \
+        --gpus=1 --gres=gpumem:70g \
+        --ntasks-per-node=1 \
+        --cpus-per-task=6 \
+        --mem-per-cpu=8G --time=1-0 \
+        --wrap="$JOB_WRAP";
+}
+
+
 
 function sbatch_gpu_big_short() {
     JOB_NAME=$1;
